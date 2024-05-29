e3a732392f9614e2b9c14017a265c88f2bac2980
Brahyam <meneses.brahyam@gmail.com>
2024-05-29 15:08:53 +0200

    updates kotlin to 2.0.0

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.kotlinAndroid)
     alias(libs.plugins.googleServices)
     alias(libs.plugins.firebaseCrashlytics)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -38,9 +39,6 @@ android {
     buildFeatures {
         compose = true
         buildConfig = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
     }
     packaging {
         resources {
